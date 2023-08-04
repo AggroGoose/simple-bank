ad68fb156223d6e3ef9646a503ab5e2d37422812
Josh Walter <102444655+StoicGoose@users.noreply.github.com>
2023-08-04 12:08:25 -0600

    Initialization of SimpleBank project with Postgres and Rest API

---
+++ pkg/util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config loads values from viper from a config file or env file to provide across the app.
+type Config struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configuration from the files or env.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
---
+++ sqlc/main_test.go
@@ -0,0 +1,30 @@
+package sqlc
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/StoicGoose/simple-bank/pkg/util"
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+var testDb *sql.DB
+
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+	testDb, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+
+	testQueries = New(testDb)
+
+	os.Exit(m.Run())
+}
+
